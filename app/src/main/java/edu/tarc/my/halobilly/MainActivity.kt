eb634608faeeb3bbe383656f9ff6f6930522e07e
Billy <gansm-wm19@student.tarc.edu.my>
2022-07-04 14:39:33 +0800

    initial commit

---
+++ app/src/main/java/edu/tarc/my/halobilly/MainActivity.kt
@@ -0,0 +1,21 @@
+package edu.tarc.my.halobilly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.show_message_button)
+        val text = findViewById<TextView>(R.id.textViewName)
+
+        button.setOnClickListener{
+            text.text = "billy"
+        }
+
+    }
+}
